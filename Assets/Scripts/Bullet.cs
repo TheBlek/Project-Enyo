bddc812fb7de83677a76f577fa7a94124d817044
Egor <egorka99966@gmail.com>
2020-12-08 15:15:51 +0700

    Now damage is registraring and bullets is not passing though walls

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float damage;
 
     private void Start()
     {
-        Destroy(this.gameObject, 3);
+        Destroy(gameObject, 3);
+        transform.GetComponent<Rigidbody>().AddForce(transform.up * speed);
     }
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        transform.Translate(Vector3.up * speed * Time.deltaTime);
+        GameObject obj = collision.collider.gameObject;
+        Debug.Log(obj.name);
+        try
+        {
+            obj.GetComponent<Damagable>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+        catch { };
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject obj = other.gameObject;
+        Debug.Log(obj.name);
+        try
+        {
+            obj.GetComponent<Damagable>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+        catch { };
     }
 }
