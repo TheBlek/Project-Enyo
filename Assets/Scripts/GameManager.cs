bbd2cf23c03eac1f184a359e446cd7173ef1dcb2
Egor <egorka99966@gmail.com>
2020-12-17 15:35:39 +0700

    Grid remembers every building on each cell

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,9 @@ public class GameManager : MonoBehaviour
     [SerializeField] private int metals;
     [SerializeField] private Builder builder;
     [SerializeField] private PlayerControls player;
-    [SerializeField] private Vector2 map_size;
+    [SerializeField] private Vector2Int map_size;
 
+    private Vector2 grid_origin;
     private List<Building> buildings;
     private List<Bounds> buildings_bounds;
 
@@ -20,7 +21,22 @@ public class GameManager : MonoBehaviour
         buildings_bounds = new List<Bounds>();
         buildings = new List<Building>();
 
-        grid = new GridCell[(int)map_size.x, (int)map_size.y];
+        InitGrid();
+    }
+
+    private void InitGrid() 
+    {
+        grid = new GridCell[map_size.x, map_size.y];
+        grid_origin = -Vector2.one * cell_size * map_size / 2;
+        
+        for (int i = 0; i < map_size.x; i++)
+        {
+            for (int j = 0; j < map_size.y; j++)
+            {
+                grid[i, j] = new GridCell();
+            }
+        }
+
     }
 
     private Vector2Int[] GetGridCellsIndexFromBuilding(Building building)
@@ -28,13 +44,13 @@ public class GameManager : MonoBehaviour
 
         Vector2 center_pos = building.transform.position;
         Vector2 left_corner_pos = center_pos - building.GetSize() * cell_size / 2;
-        Vector2 right_corner_pos = center_pos + building.GetSize() * cell_size / 2;
+        Vector2 size = building.GetSize();
         List<Vector2Int> cells = new List<Vector2Int>();
-        for (float i = left_corner_pos.x; i < right_corner_pos.x; i += cell_size)
+        for (int i = 0; i < size.x; i++)
         {
-            for (float j = left_corner_pos.y; j < right_corner_pos.y; j += cell_size)
+            for (int j = 0; j < size.x; j++)
             {
-                cells.Add(GetGridCellIndexFromCoords(new Vector2(i, j)));
+                cells.Add(GetGridCellIndexFromCoords(left_corner_pos + new Vector2(i,j) * cell_size));
             }
         }
         return cells.ToArray();
@@ -42,15 +58,15 @@ public class GameManager : MonoBehaviour
 
     private Vector2Int GetGridCellIndexFromCoords(Vector2 coords)
     {
-        return new Vector2Int((int)coords.x, (int)coords.y);
+        return new Vector2Int((int)((coords.x - grid_origin.x)/cell_size), (int)((coords.y - grid_origin.y) / cell_size));
     }
 
     private void AdjustCellsForBuilding(Building building)
     {
-
         Vector2Int[] cells = GetGridCellsIndexFromBuilding(building);
         foreach (Vector2Int cell in cells)
         {
+            Debug.Log("Changing state of cell on x: " + cell.x + ", y: " + cell.y);
             grid[cell.x, cell.y].SetBuilding(building);
         }
 
@@ -64,6 +80,7 @@ public class GameManager : MonoBehaviour
 
         buildings.Add(building);
         buildings_bounds.Add(building.GetComponent<BoxCollider2D>().bounds);
+        AdjustCellsForBuilding(building);
     }
     
     public bool IsThereAWall(Vector3 pos)
