fb7ff524b357766d13372fa847a67e56849b2ed1
Egor <egorka99966@gmail.com>
2020-12-22 15:54:55 +0700

    Added shots per second property

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -7,11 +7,31 @@ public class Shooter : MonoBehaviour
     [SerializeField] private Transform shoot_pos;
     [SerializeField] private GameObject bullet_prefab;
     [SerializeField] private GameObject muzzleflash_prefab;
+    [SerializeField] private float fires_per_sec;
+
+    private float delay;
+    private float time_since_last_shot;
+
+    private void Start()
+    {
+        delay = 1/fires_per_sec;
+    }
+
+    private void Update()
+    {
+        time_since_last_shot += Time.deltaTime;
+    }
 
     public void Shoot()
     {
+        if (time_since_last_shot < delay)
+            return;
+
         Instantiate(bullet_prefab, shoot_pos.position, shoot_pos.rotation);
+
         var muzzleflash = Instantiate(muzzleflash_prefab, shoot_pos.position, Quaternion.Euler(shoot_pos.rotation.eulerAngles));
         Destroy(muzzleflash, 0.5f);
+
+        time_since_last_shot -= delay;
     }
 }
