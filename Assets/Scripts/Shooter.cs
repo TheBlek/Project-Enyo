6bef820b7bbdbce443a99b6d4856273b1c04faee
Egor <egorka99966@gmail.com>
2020-12-22 15:49:52 +0700

    Added muzzleflash to shooting

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,9 +6,12 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] private Transform shoot_pos;
     [SerializeField] private GameObject bullet_prefab;
+    [SerializeField] private GameObject muzzleflash_prefab;
 
     public void Shoot()
     {
         Instantiate(bullet_prefab, shoot_pos.position, shoot_pos.rotation);
+        var muzzleflash = Instantiate(muzzleflash_prefab, shoot_pos.position, Quaternion.Euler(shoot_pos.rotation.eulerAngles));
+        Destroy(muzzleflash, 0.5f);
     }
 }
