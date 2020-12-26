23542b2f13138678b6c3515295aa9a2f681ccb15
Egor <egorka99966@gmail.com>
2020-12-26 11:05:27 +0700

    Redrew bullet, blowing animation implemented

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,12 +6,15 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float damage;
-    [SerializeField] private float live_time;
+    [SerializeField] private float life_time;
+
+    private Animator animator;
 
     private void Start()
     {
-        Destroy(gameObject, live_time);
+        Destroy(gameObject, life_time);
         transform.GetComponent<Rigidbody2D>().AddForce(transform.right * speed);
+        animator = transform.GetComponent<Animator>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -19,7 +22,8 @@ public class Bullet : MonoBehaviour
         try
         {
             obj.GetComponent<Damagable>().TakeDamage(damage);
-            Destroy(gameObject);
+            HandleBlowAnimation();
+            Destroy(gameObject, .5f);
         }
         catch { };
     }
@@ -30,8 +34,15 @@ public class Bullet : MonoBehaviour
         try
         {
             obj.GetComponent<Damagable>().TakeDamage(damage);
-            Destroy(gameObject);
+            HandleBlowAnimation();
+            Destroy(gameObject, .5f);
         }
         catch { };
+    }
+
+    private void HandleBlowAnimation()
+    {
+        transform.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        animator.Play("Blow");
     }
 }
