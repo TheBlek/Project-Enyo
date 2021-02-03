189d77ab0fdfc3531d2f2cf031ea89f34394cd48
Egor <egorka99966@gmail.com>
2021-02-03 17:18:03 +0700

    Drew sprites for ui + little tab system

---
+++ Assets/Scripts/UI/TabButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+class TabButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Action<TabButton> OnClick;
+    public Action<TabButton> OnEnter;
+    public Action<TabButton> OnExit;
+
+    public void ChangeSprite(Sprite sprite)
+    {
+        GetComponent<Image>().sprite = sprite;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OnClick(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        OnEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        OnExit(this);
+    }
+}
