2fd1f2d24fab1813d605b40d90854a81900cc662
Egor <egorka99966@gmail.com>
2021-02-22 18:01:26 +0700

    Reworked corner adjustments to rule tiles with some extension like priority

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -6,7 +6,7 @@ public class MapManager : GridManager<MapCell>
 {
     [SerializeField] private Tilemap tilemap;
     [SerializeField] private MapTiles[] keys;
-    [SerializeField] private Tile[] values;
+    [SerializeField] private RuleTile[] values;
 
     [SerializeField] private float start_freqency;
     [SerializeField] private float lacunarity;
@@ -14,10 +14,7 @@ public class MapManager : GridManager<MapCell>
     [SerializeField] private float persistance;
     [SerializeField] private int octaves_count;
 
-    [SerializeField] private Tile[] tile_patterns_grass;
-    [SerializeField] private Tile[] tile_patterns_ground;
-
-    private Dictionary<MapTiles, Tile> tiles_by_name;
+    private Dictionary<MapTiles, RuleTile> tiles_by_name;
 
     
     public override void InitGrid()
@@ -26,12 +23,11 @@ public class MapManager : GridManager<MapCell>
         GenerateMap();
         ConvertArrayToDict();
         SetUpLayout();
-        AdjustCornerTiles();
     }
     
     private void ConvertArrayToDict()
     {
-        tiles_by_name = new Dictionary<MapTiles, Tile>();
+        tiles_by_name = new Dictionary<MapTiles, RuleTile>();
 
         for (int i = 0; i < keys.Length; i++)
         {
@@ -47,33 +43,6 @@ public class MapManager : GridManager<MapCell>
             for (int y = 0; y < grid_size.y; y++)
             {
                 tilemap.SetTile(new Vector3Int(x - grid_size.x/2, y - grid_size.y / 2, 0), tiles_by_name[grid[x, y].Tile]);
-            }
-        }
-    }
-
-    private void AdjustCornerTiles()
-    {
-        NeighbourAdjuster<Tile, MapCell> ground_adjuster = new NeighbourAdjuster<Tile, MapCell>(tile_patterns_ground);
-        NeighbourAdjuster<Tile, MapCell> grass_adjuster = new NeighbourAdjuster<Tile, MapCell>(tile_patterns_grass);
-        for (int x = 0; x < grid_size.x; x++)
-        {
-            for (int y = 0; y < grid_size.y; y++)
-            {
-                MapCell[] neighbours = GetStraightNeighbours(new Vector2Int(x, y));
-
-                Tile new_tile = tiles_by_name[grid[x, y].Tile];
-                switch (grid[x, y].Tile)
-                {
-                    case MapTiles.Grass:
-                        new_tile = grass_adjuster.GetResultForSubject(grid[x, y], neighbours);
-                        break;
-                    case MapTiles.Ground:
-                        new_tile = ground_adjuster.GetResultForSubject(grid[x, y], neighbours);
-                        break;
-                    default:
-                        break;
-                }
-                tilemap.SetTile(new Vector3Int(x - grid_size.x / 2, y - grid_size.y / 2, 0), new_tile);
             }
         }
     }
