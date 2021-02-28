f6c9db0d3667bcc9eb45ac7b93d09120fce26845
Egor <egorka99966@gmail.com>
2021-02-28 18:20:25 +0700

    Reorganized PNG files + added blow animation for cannon

--- Assets/Scripts/Buildings/Cannon.cs
+++ Assets/Scripts/Buildings/Cannon.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 
 public class Cannon : AttackBuilding
 {
-    private Animator _animator;
+    [SerializeField] private Animator _blow_animator;
+    private Animator _shoot_animator;
 
     private new void Start()
     {
         base.Start();
-        _animator = _barrel.GetComponent<Animator>();
+        _shoot_animator = _barrel.GetComponent<Animator>();
         _shooter.OnShoot += PlayShootAnimation;
+
+        var d = GetComponent<Damagable>();
+        d.death_offset = .25f;
+        d.onKill += PlayBlowAnimation;
     }
 
     private void PlayShootAnimation()
     {
-        _animator.Play("Shoot");
+        _shoot_animator.Play("Shoot");
+    }
+
+    private void PlayBlowAnimation()
+    {
+        _blow_animator.Play("Blow");
     }
 
 }
