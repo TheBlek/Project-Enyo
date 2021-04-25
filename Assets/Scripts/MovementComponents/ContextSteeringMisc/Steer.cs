84a12dc30341a1a7ba658fe6f213b81062ddd615
Egor <egorka99966@gmail.com>
2021-04-25 17:17:56 +0700

    Small fix in behaviour - now they don't walk on virtual direction with danger

--- Assets/Scripts/MovementComponents/ContextSteeringMisc/Steer.cs
+++ Assets/Scripts/MovementComponents/ContextSteeringMisc/Steer.cs
@@ -61,10 +61,10 @@ public struct Steer
         Gizmos.color = Color.blue;
         Gizmos.DrawLine(position, position + GetTheBestDirection());
         Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(position, position + GetResultDirection());
+        Gizmos.DrawLine(position, position + GetTheBestDirection(false));
     }
 
-    public Vector2 GetTheBestDirection()
+    public Vector2 GetTheBestDirection(bool use_gradients=true)
     {
         if (DirectionsCount == 0)
             return Vector2.zero;
@@ -84,70 +84,74 @@ public struct Steer
 
         if (max_index == -1) return Vector2.zero;
 
-        Vector2 virtual_one = CalculateSubslotBestOption(max_index);
+        Vector2 virtual_one = CalculateSubslotBestOption(max_index, minimal_danger);
         Vector2 actual_one = _context_map.Directions[max_index] * _context_map.Interests[max_index];
 
-        if (virtual_one == Vector2.zero) return actual_one;
+        if (virtual_one == Vector2.zero || !use_gradients) return actual_one;
 
         return actual_one.magnitude > virtual_one.magnitude ? actual_one   : virtual_one;
     }
 
-    private Vector2 CalculateSubslotBestOption(int index)
+    private Vector2 CalculateSubslotBestOption(int index, float min_danger)
     {
         int index_leftest = WrapIndexToNormal(index - 2);
         int index_left = WrapIndexToNormal(index - 1);
         int index_right = WrapIndexToNormal(index + 1);
         int index_rightest = WrapIndexToNormal(index + 2);
-        Vector2 point0 = new Vector2(-2f, _context_map.Interests[index_leftest]);
-        Vector2 point1 = new Vector2(-1f, _context_map.Interests[index_left]);
-        Vector2 point2 = new Vector2(0f, _context_map.Interests[index]);
-        Vector2 point3 = new Vector2(1f, _context_map.Interests[index_right]);
-        Vector2 point4 = new Vector2(2f, _context_map.Interests[index_rightest]);
+        Vector2 point0 = new Vector2(-2f, _context_map.Dangers[index_leftest] == min_danger ? _context_map.Interests[index_leftest] : 0);
+        Vector2 point1 = new Vector2(-1f, _context_map.Dangers[index_left] == min_danger ? _context_map.Interests[index_left] : 0);
+        Vector2 point2 = new Vector2(0f, _context_map.Dangers[index] == min_danger ? _context_map.Interests[index] : 0);
+        Vector2 point3 = new Vector2(1f, _context_map.Dangers[index_right] == min_danger ? _context_map.Interests[index_right] : 0);
+        Vector2 point4 = new Vector2(2f, _context_map.Dangers[index_rightest] == min_danger ? _context_map.Interests[index_rightest] : 0);
 
         //This is representation of line equation: y = ax + b, where a is line.x and b is line.y
         //right gradient
         Vector2 line1 = new Vector2(InclineOfLine(point1, point2), point1.y - InclineOfLine(point1, point2) * point1.x);
         Vector2 line2 = new Vector2(InclineOfLine(point3, point4), point3.y - InclineOfLine(point3, point4) * point3.x);
 
-        Vector2 virtual_right = Vector2.zero;
+        Vector2 intersection_point = LinesIntersection(line1, line2);
 
-        if (line1.x != line2.x)
-        {
-            float virtual_slot = (line2.y - line1.y) / (line1.x - line2.x);
-            if (Mathf.Abs(virtual_slot) > 2f)
-                virtual_slot = 0;
+        if (Mathf.Abs(intersection_point.x) > 2f || _context_map.Dangers[WrapIndexToNormal((int)intersection_point.x)] != min_danger)
+            intersection_point.x = 0;
 
-            Vector2 virtual_direction_right = Vector2.LerpUnclamped(_context_map.Directions[index], _context_map.Directions[index_right], virtual_slot);
+        Vector2 virtual_direction = Vector2.LerpUnclamped(_context_map.Directions[index], _context_map.Directions[index_right], intersection_point.x);
 
-            virtual_right = virtual_direction_right * (line1.x * virtual_slot + line1.y);
-        }
+        Vector2 virtual_right = virtual_direction * intersection_point.y;
 
         //left gradient
         Vector2 line3 = new Vector2(InclineOfLine(point0, point1), point0.y - InclineOfLine(point0, point1) * point0.x);
         Vector2 line4 = new Vector2(InclineOfLine(point2, point3), point2.y - InclineOfLine(point2, point3) * point2.x);
 
-        Vector2 virtual_left = Vector2.zero;
+        intersection_point = LinesIntersection(line3, line4);
 
-        if (line3.x != line4.x)
-        {
-            float virtual_slot = (line4.y - line3.y) / (line3.x - line4.x);
-            if (Mathf.Abs(virtual_slot) > 2f)
-                virtual_slot = 0;
+        if (Mathf.Abs(intersection_point.x) > 2f || _context_map.Dangers[WrapIndexToNormal((int)intersection_point.x)] != min_danger)
+            intersection_point.x = 0;
 
-            Vector2 virtual_direction_left = Vector2.LerpUnclamped(_context_map.Directions[index], _context_map.Directions[index_right], virtual_slot);
+        virtual_direction = Vector2.LerpUnclamped(_context_map.Directions[index], _context_map.Directions[index_right], intersection_point.x);
 
-            virtual_left = virtual_direction_left * (line3.x * virtual_slot + line3.y);
-        }
+        Vector2 virtual_left = virtual_direction * intersection_point.y;
 
-        Vector2 virtual_result =virtual_left.magnitude > virtual_right.magnitude && virtual_left.magnitude < 3f ? 
-                                virtual_left : 
-                                (virtual_right.magnitude < 3f) ? virtual_right : Vector2.zero;
-
-        return virtual_result;
+        return virtual_left.magnitude > virtual_right.magnitude && virtual_left.magnitude < 3f ?
+                                 virtual_left :
+                                 (virtual_right.magnitude < 3f) ? virtual_right : Vector2.zero;
 
         float InclineOfLine(Vector2 p1, Vector2 p2)
         {
             return (p2.y - p1.y) / (p2.x - p1.x);
+        }
+
+        Vector2 LinesIntersection(Vector2 l1, Vector2 l2)
+        {
+            if (l1.x == l2.x) return Vector2.zero;
+
+            float intersection = (l2.y - l1.y) / (l1.x - l2.x);
+
+            return new Vector2(intersection, LineValue(l1, intersection));
+        }
+
+        float LineValue(Vector2 l, float x)
+        {
+            return x * l.x + l.y;
         }
     }
 
